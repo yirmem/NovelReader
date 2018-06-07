fe2c1c892afd6b769ee291ea4dee319377d5d4e3
盛斌 <shengbin@shengbindeMacBook-Pro.local>
2018-06-07 13:57:32 +0800

    6-07 13:57

--- index.js
+++ index.js
@@ -1,50 +1,5 @@
 // 引入electron模块  
 const electron = require('electron');
-var http = require('http');
-var url = require('url');
-var util = require('util');
-var cheerio = require('cheerio');
-var querystring = require('querystring');
-var fs = require('fs');
-
-var urlStr = 'http://www.baidu.com/s?wd='+'放开那个女巫'+'&pn=30&rsv_spt=1&rsv_iqid=0xdd2c31de0003d13c&issp=1&f=3&rsv_bp=0&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=0&rsv_sug3=19&rsv_sug1=16&rsv_sug7=100&prefixsug='+'放开那个女巫'+'&rsp=8&inputT=6040&rsv_sug4=7275'
-
-var $ = cheerio.load(fs.readFileSync('index.html'))
-
-$(".content").text('134134342')
-
-var data = {    
-    wd:'放开那个女巫', 
-    pn:30,
-    rsv_spt:1,
-    rsv_iqid:'0xdd2c31de0003d13c&issp=1'
-};//这是需要提交的数据    
-    
-    
-var content = querystring.stringify(data);    
-    
-var options = {    
-    hostname: 'http://www.baidu.com',    
-    port: 80,
-    path: '/s?' + content,    
-    method: 'GET'    
-};    
-    
-var req = http.request(options, function (res) {    
-    console.log('STATUS: ' + res.statusCode);    
-    console.log('HEADERS: ' + JSON.stringify(res.headers));    
-    res.setEncoding('utf8');    
-    res.on('data', function (chunk) {    
-        //console.log('BODY: ' + chunk);    
-        $('.content').text(chunk)
-    });    
-});    
-    
-req.on('error', function (e) {    
-    console.log('problem with request: ' + e.message);    
-});    
-    
-req.end(); 
 
 // 用来控制应用的生命周期  
 const app = electron.app;
@@ -64,6 +19,7 @@ function createWindow() {
   })
 //加载项目目录下的index.html
 mainWindow.loadURL('file://' + __dirname + '/index.html');
+mainWindow.webContents.openDevTools()
 // 当窗口被关闭时调用
 mainWindow.on('closed', function () {
   // 取消引用window对象，如果你的应用支持多窗口的话，通常
@@ -74,6 +30,22 @@ mainWindow.on('closed', function () {
 // 当应用触发 ready 事件后，创建浏览器窗口
 app.on('ready',function(){
  createWindow();
+
+ //const novelContent = document.getElementsByClass("content")[0]
+
+ const {net} = require('electron')
+ const request = net.request('http://www.baidu.com/s?wd=放开那个女巫&pn=10&ie=utf-8&tn=baiduhome_pg')
+    request.on('response', (response) => {
+      console.log(`STATUS: ${response.statusCode}`)
+      console.log(`HEADERS: ${JSON.stringify(response.headers)}`)
+      response.on('data', (chunk) => {
+        console.log(`BODY: ${chunk}`)
+      })
+      response.on('end', () => {
+        console.log('response请求中没有更多数据。')
+      })
+    })
+    request.end()
 })
 
 // 当点击关闭时，在dock中保留electron
